c916c9f58302b9292fc9c03dbf877fa05ac40746
long.vuduc <vuduclong0309@gmail.com>
2022-10-03 22:13:16 +0800

    Added Jacob Sample file

--- Jacob_IBKR_API_Bot.py
+++ Jacob_IBKR_API_Bot.py
@@ -76,7 +76,7 @@ class Bot:
     def __init__(self):
         #Connect to IB on init
         self.ib = IBApi()
-        self.ib.connect("127.0.0.1", 7496,1)
+        self.ib.connect("127.0.0.1", 7497,1)
         ib_thread = threading.Thread(target=self.run_loop, daemon=True)
         ib_thread.start()
         time.sleep(1)
