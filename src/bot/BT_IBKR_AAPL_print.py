fe2af8e9fd5af8e3bc5cbfa01675c79263f308d2
long.vuduc <vuduclong0309@gmail.com>
2022-10-05 23:56:51 +0800

    created bot for livetrading

---
+++ src/bot/BT_IBKR_AAPL_print.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+###############################################################################
+#
+# Copyright (C) 2018 Daniel Rodriguez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import datetime
+
+import backtrader as bt
+import backtrader.indicators as btind
+import backtrader.feeds as btfeeds
+
+
+class SmaCross(bt.Strategy):
+    # list of parameters which are configurable for the strategy
+    params = dict(
+        pfast=10,  # period for the fast moving average
+        pslow=30   # period for the slow moving average
+    )
+
+    def logdata(self):
+        txt = []
+        txt.append('{}'.format(len(self)))
+        txt.append('{}'.format(self.data.datetime.datetime(0).isoformat()))
+        txt.append('{:.2f}'.format(self.data.open[0]))
+        txt.append('{:.2f}'.format(self.data.high[0]))
+        txt.append('{:.2f}'.format(self.data.low[0]))
+        txt.append('{:.2f}'.format(self.data.close[0]))
+        txt.append('{:.2f}'.format(self.data.volume[0]))
+        print(','.join(txt))
+
+    def __init__(self):
+        sma1 = bt.ind.SMA(period=self.p.pfast)  # fast moving average
+        sma2 = bt.ind.SMA(period=self.p.pslow)  # slow moving average
+        self.crossover = bt.ind.CrossOver(sma1, sma2)  # crossover signal
+
+    def next(self):
+        self.logdata()
+
+        if not self.position:  # not in the market
+            if self.crossover > 0:  # if fast crosses slow to the upside
+                self.buy()  # enter long
+
+        elif self.crossover < 0:  # in the market & cross to the downside
+            self.close()  # close long position
+
+    def notify_data(self, data, status, *args, **kwargs):
+        print('*' * 5, 'DATA NOTIF:', data._getstatusname(status), *args)
+        if status == data.LIVE:
+            self.data_live = True
+
+def run(args=None):
+
+    cerebro = bt.Cerebro(stdstats=False)
+    cerebro.addstrategy(SmaCross)
+    store = bt.stores.IBStore(port=7497)
+    stockkwargs = dict(
+        timeframe=bt.TimeFrame.Minutes,
+        rtbar=False,  # use RealTime 5 seconds bars
+        historical=True,  # only historical download
+        qcheck=0.5,  # timeout in seconds (float) to check for events
+        #fromdate=datetime.datetime(2021, 9, 24),  # get data from..
+        #todate=datetime.datetime(2022, 9, 25),  # get data from..
+        latethrough=False,  # let late samples through
+        tradename=None  # use a different asset as order target
+    )
+    data0 = store.getdata(dataname="AAPL-STK-SMART-USD", **stockkwargs)
+    cerebro.resampledata(data0, timeframe=bt.TimeFrame.Minutes, compression=1)
+    stval = cerebro.broker.getvalue()
+    cerebro.run()
+    endval = cerebro.broker.getvalue()
+    print(stval)
+    print(endval)
+
+
+if __name__ == '__main__':
+    run()
