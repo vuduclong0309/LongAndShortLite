9c5d00377cdddb9b2e7f8407cba7b010057741f2
long.vuduc <vuduclong0309@gmail.com>
2022-10-12 01:39:24 +0800

    added multifeed example and option buy testing

--- spike_sample_files/BT_IBKR_option_sample.py
+++ spike_sample_files/BT_IBKR_option_sample.py
@@ -21,10 +21,23 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import datetime
+
 import backtrader as bt
+import backtrader.indicators as btind
+import backtrader.feeds as btfeeds
 
 
-class St(bt.Strategy):
+class StochRSI(bt.Strategy):
+
+    def __init__(self):
+        self.rsi = bt.indicators.RSI_SMA(self.data.close, period=14)
+
+    # outputting information
+    def log(self, txt):
+        dt=self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
     def logdata(self):
         txt = []
         txt.append('{}'.format(len(self)))
@@ -38,6 +51,21 @@ class St(bt.Strategy):
 
     data_live = False
 
+    def next(self):
+        self.logdata()
+        #if self.data_live == False:
+        #    return
+
+        #if not self.position: # check if you already have a position in the market
+        if (self.rsi < 30 and self.position.size < 100):
+            self.log('Buy Create, %.2f' % self.datas[1].close[0])
+            self.buy(data="d1", size=1) # buy when closing price today crosses above MA.
+        else:
+            # This means you are in a position, and hence you need to define exit strategy here.
+            if (self.rsi > 70 and self.position.size > -100):
+                self.log('Position Closed, %.2f' % self.datas[1].close[0])
+                self.sell(data="d1", size=1)
+
     def notify_data(self, data, status, *args, **kwargs):
         print('*' * 5, 'DATA NOTIF:', data._getstatusname(status), *args)
         if status == data.LIVE:
@@ -45,31 +73,18 @@ class St(bt.Strategy):
 
     def notify_order(self, order):
         if order.status == order.Completed:
-            buysell = 'BUY ' if order.isbuy() else 'SELL'
-            txt = '{} {}@{}'.format(buysell, order.executed.size,
-                                    order.executed.price)
-            print(txt)
-
-    def notify_store(self, msg, *args, **kwargs):
-        print('STORE NOTIF:{}', msg)
-
-    bought = 0
-    sold = 0
-
-    def next(self):
-        print("ok")
-        self.logdata()
-        if not self.data_live:
-            return
-
-        print(self.datas)
-
-        #if not self.bought:
-        #    self.buy(data="d1",size=1) # buy when closing price today crosses above MA.
-        #elif not self.sold:
-        #    if len(self) == (self.bought + 3):
-        #        self.sell()
-
+            if order.isbuy():
+                self.log(
+                "Executed BUY (Price: %.2f, Value: %.2f, Commission %.2f)" %
+                (order.executed.price, order.executed.value, order.executed.comm))
+            else:
+                self.log(
+                "Executed SELL (Price: %.2f, Value: %.2f, Commission %.2f)" %
+                (order.executed.price, order.executed.value, order.executed.comm))
+                self.bar_executed = len(self)
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log("Order was canceled/margin/rejected")
+            self.order = None
 
 def run(args=None):
 
@@ -85,17 +100,23 @@ def run(args=None):
         latethrough=False,  # let late samples through
         tradename=None  # use a different asset as order target
     )
-    data0 = store.getdata(dataname="AAPL-STK-SMART-USD", **stockkwargs)
+    data0 = store.getdata(dataname="SPY-STK-SMART-USD", **stockkwargs)
     data1 = store.getdata(dataname="SPY-20221014-SMART-USD-366-PUT", **stockkwargs)
-    
     cerebro.adddata(data0, name='d0')
     cerebro.adddata(data1, name='d1')
-    #cerebro.resampledata(data0, timeframe=bt.TimeFrame.Seconds, compression=5)
+    #cerebro.resampledata(data0, timeframe=bt.TimeFrame.Minutes, compression=3)
+    stval = cerebro.broker.getvalue()
 
     #cerebro.broker = store.getbroker()
+    stval = cerebro.broker.getvalue()
 
-    cerebro.addstrategy(St)
+    cerebro.addstrategy(StochRSI)
     cerebro.run()
+
+    endval = cerebro.broker.getvalue()
+
+    print(stval)
+    print(endval)
     cerebro.plot()
 
 
