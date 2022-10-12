4a2010c663faf8236216b999deaeaa08798bede2
long.vuduc <vuduclong0309@gmail.com>
2022-10-12 23:48:25 +0800

    added rsi pair trading

--- spike_sample_files/ibtest.py
+++ spike_sample_files/ibtest.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8; py-indent-offset:4 -*-
+# python ibtest.py --port 7497 --qcheck 2 --reconnect 3 --data0 EUR.USD-CASH-IDEALPRO --timeframe Seconds --compression 10
 ###############################################################################
 #
 # Copyright (C) 2015-2020 Daniel Rodriguez
