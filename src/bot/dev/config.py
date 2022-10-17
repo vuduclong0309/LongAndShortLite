8b7cfd19f91eb2e2b2df6a0208dbb8bef7f69196
long.vuduc <vuduclong0309@gmail.com>
2022-10-18 00:18:34 +0800

    move inactive file to icebox, refactor config for dev

--- src/bot/dev/config.py
+++ src/bot/dev/config.py
@@ -1,3 +1,16 @@
+import backtrader as bt
+import backtrader.indicators as btind
+import backtrader.feeds as btfeeds
+
+#IBKR Client parameters
+port_conf=7497
+p_factor = 1  # IBKR simul broker has option price *100, thus need to divide by 100 to make price back to normal
+use_rt_bar = False
+
+# Trade parameters
 backtest_glob = True
 symbol_glob = "SPY"
-port_conf=7497
+trade_timeframe_type = bt.TimeFrame.Minutes
+trade_timeframe_compress = 1
+
+  
