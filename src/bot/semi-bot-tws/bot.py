994175b8c30d94189e855edeeb08e58274bb4e7a
long.vuduc <vuduclong0309@gmail.com>
2022-11-04 23:19:59 +0800

    TWS API bot

---
+++ src/bot/semi-bot-tws/bot.py
@@ -0,0 +1,33 @@
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+from ibapi.contract import Contract
+
+class OrderApp(EWrapper, EClient):
+    def __init__(self):
+        EClient.__init__(self, self)
+
+    def error(self, reqId, errorCode, errorString):
+        print("Error: ", reqId, " ", errorCode, " ", errorString)
+
+    def contractDetails(self, reqId, contractDetails):
+        print("contractDetails: ", reqId, " ", contractDetails)
+
+
+def main():
+    app = OrderApp()
+
+    app.connect("127.0.0.1", 7497, 0)
+
+    contract = Contract()
+    contract.symbol = "AAPL"
+    contract.secType = "STK"
+    contract.exchange = "SMART"
+    contract.currency = "USD"
+    contract.primaryExchange = "NASDAQ"
+
+    app.reqContractDetails(1, contract)
+
+    app.run()
+
+if __name__ == "__main__":
+    main()
