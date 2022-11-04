5158b4c5135c5009b17030d8c7122017e68d3718
long.vuduc <vuduclong0309@gmail.com>
2022-11-04 23:57:32 +0800

    add historical data code

--- src/bot/semi-bot-tws/marketData.py
+++ src/bot/semi-bot-tws/marketData.py
@@ -1,10 +1,17 @@
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
 from ibapi.contract import Contract
+from ibapi.ticktype import TickTypeEnum
 
 class OrderApp(EWrapper, EClient):
     def __init__(self):
         EClient.__init__(self, self)
+
+    def tickPrice(self, reqId, tickType, price, attrib):
+        print("Tick Price. Ticker Id:", reqId, "tickType:", TickTypeEnum.to_str(tickType))
+
+    def tickSize(self, reqId, tickType, size):
+        print("Tick Size. Ticker Id:", reqId, "tickType", TickTypeEnum.to_str(tickType), "Size:", size)
 
     def error(self, reqId, errorCode, errorString):
         print("Error: ", reqId, " ", errorCode, " ", errorString)
@@ -25,7 +32,9 @@ def main():
     contract.currency = "USD"
     contract.primaryExchange = "NASDAQ"
 
-    app.reqContractDetails(1, contract)
+    #app.reqContractDetails(1, contract)
+    app.reqMarketDataType(4)
+    app.reqMktData(1, contract, "", False, False, [])
 
     app.run()
 
